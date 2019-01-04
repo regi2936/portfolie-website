af2639ffd8ffff7ecb0d03c9acfce16aab92f852
Regitze Philipsen <regitzephilipsen@laptop-9bg8d6hu.kea.local>
2019-01-04 10:53:00 +0100

    test

---
+++ script.js
@@ -0,0 +1,24 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.querySelectorAll(".mySlides");
+    let dots = document.querySelectorAll(".dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+
+
+/******** Dette script er fundet på : https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow_auto********/
